4aeea65e027df159cc93cb6cc3cd87cef5a5ee78
a4ashish8 <92498926+a4ashish8@users.noreply.github.com>
2024-09-30 09:21:17 +0530

    chng in error jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
 // src/App.js
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import UserList from './pages/UserList'; // Ensure the path to Login component is correct
+import UserList from './pages/UserList'; // Ensure the path to Login component is correct
 import Login from './pages/Login';
+import Error from './pages/Error';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path='/allUser' element={<UserList />} />
         {/* Add other routes here */}
+
+        <Route path='/*' element={<Error />} />
       </Routes>
     </div>
   );
