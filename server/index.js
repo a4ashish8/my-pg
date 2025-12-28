62510c03bf1267b6308a858ace2d1cfe6db5ebdf
a4ashish8 <92498926+a4ashish8@users.noreply.github.com>
2025-12-28 23:50:57 +0530

    Fix CORS origin URL formatting

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-	  origin: ["https://my-pg.netlify.app", "http://localhost:3000", "http://http://13.62.100.172/:3000"],
+	  origin: ["https://my-pg.netlify.app", "http://localhost:3000", "http://http://13.62.100.172:3000"],
 	  credentials: true,
 	})
   );
