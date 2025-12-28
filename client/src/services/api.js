be9397cca2bdfe42e77e6ccbcfb8d52d8fe56e34
a4ashish8 <92498926+a4ashish8@users.noreply.github.com>
2025-12-28 23:35:42 +0530

    Ashish commit (#29)
    * modal update payment
    
    * aaa
    
    * push
    
    * mail
    
    * Refactor user registration process and update email sender details
    
    * Update api.js
    
    * build
    
    * css
    
    * s
    
    * as
    
    * chnage url
    
    * routes
    
    * chnage in api conector
    
    * new build
    
    * build delete
    
    * build
    
    * Create favicon.ico
    
    * test add url aws
    
    * chnage base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8000/api/v1"
+// const BASE_URL = "http://localhost:8000/api/v1"
+const BASE_URL = "http://http://13.62.100.172/:8000/api/v1"
 
 
 // USER ENDPOINTS
